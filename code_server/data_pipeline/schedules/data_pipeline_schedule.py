c3e9b248873bbd016bfcf595019ec93a31c04502
aedan chiari <aedanc00@gmail.com>
2025-12-06 16:57:52 -0700

    Update daily stock schedule to include job name, run config, and execution timezone

--- code_server/data_pipeline/schedules/data_pipeline_schedule.py
+++ code_server/data_pipeline/schedules/data_pipeline_schedule.py
@@ -1,7 +1,11 @@
 from dagster import ScheduleDefinition
+from code_server.data_pipeline.data_pipeline_config import partitioned_config
+
 
 daily_stock_schedule = ScheduleDefinition(
     name="daily_stock_price_update",
-    target="*",  # Run all assets
+    job_name="data_pipeline_job",
     cron_schedule="0 17 * * 1-5",  # 5 PM on weekdays, Monday to Friday
+    run_config_fn=partitioned_config,
+    execution_timezone="America/Denver",
 )
